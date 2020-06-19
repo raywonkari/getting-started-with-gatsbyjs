58df9edd6255fa9f941a1512d25689b55a49aaa9
Raywon Kari <raywon.teja@fortum.com>
2020-06-19 19:14:44 +0200

    add header component

---
+++ src/components/header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StaticQuery, graphql, Link } from "gatsby";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+function Header() {
+    return(
+        <StaticQuery
+            query = { graphql`
+                {
+                    allSite {
+                        edges {
+                            node {
+                                siteMetadata {
+                                    title
+                                }
+                            }
+                        }
+                    }
+                }
+            `}
+
+            render = { data => (
+                <div>
+                    {data.allSite.edges.map( ({node}) => (
+                        <div>
+                            <Navbar bg="dark" variant="dark">
+                                <Navbar.Brand href="/">
+                                    {node.siteMetadata.title}
+                                </Navbar.Brand>
+                                <Nav>
+                                    <Nav.Link href="/pagetwo"> Second Page </Nav.Link>
+                                </Nav>
+                            </Navbar>
+                        </div>
+                    ))}
+                </div>
+            )}
+        />
+    )
+}
+
+export default Header;
