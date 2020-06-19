e275a38edbf0e665783fb4d0ecc72a513930458e
Raywon Kari <raywon.teja@fortum.com>
2020-06-19 19:15:07 +0200

    add a layout component with header, footer & children

---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header.js";
+import Footer from "./footer.js";
+
+function Layout( {children} ) {
+    return(
+        <div>
+            <Header/>
+            {children}
+            <Footer/>
+        </div>
+    )
+}
+
+export default Layout;
