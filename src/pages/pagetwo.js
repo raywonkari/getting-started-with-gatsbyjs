12fb2dae32dfb15435b6b6105d74d8df18e7e722
Raywon Kari <raywon.teja@fortum.com>
2020-06-19 19:15:21 +0200

    create page two

---
+++ src/pages/pagetwo.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout.js";
+
+function pageTwo() {
+    return(
+        <div>
+            <Layout>
+            <div style={{margin: '300px', textAlign: 'center'}}>
+                <h2>This is my second page</h2>
+            </div>
+            </Layout>
+        </div>
+    )
+}
+
+export default pageTwo;
