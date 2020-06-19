f019642ca6f36be0152a788cf9021daff58ad4a2
Raywon Kari <raywon.teja@fortum.com>
2020-06-19 15:12:43 +0200

    add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'My Personal Website',
+    author: 'Raywon Kari',
+    twitter: 'https://twitter.com/raywonkari',
+    linkedin: 'https://linkedin.com/in/raywonkari',
+    github: 'https://github.com/raywonkari',
+  }
 }
