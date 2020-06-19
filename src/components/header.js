4c0384296c5a614e21bc58977eeec1ec6491762a
Raywon Kari <raywon.teja@fortum.com>
2020-06-19 19:24:55 +0200

    remove Link

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StaticQuery, graphql, Link } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
