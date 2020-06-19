3281c3310b1f934ba3ba2edd3e9c96600f61fb2e
Raywon Kari <raywon.teja@fortum.com>
2020-06-19 19:15:13 +0200

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
+import Layout from "../components/layout.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image from "../images/image.jpg";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+    <div>
+      <Layout>
+        <div style={{margin: '100px', textAlign: 'center'}}>
+          <h2>This is my home page</h2>
+          <br/>
+          <img src={image} alt="coverimage"></img>
+          <br/>
+          Photo by <a href="https://unsplash.com/@igormiske?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Igor Miske</a> on <a href="https://unsplash.com/s/photos/website?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </div>
+      </Layout>
+    </div>
+  )
 }
